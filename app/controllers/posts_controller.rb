ab1e65c05677c9157356c65586112694767715a8
Malissa Cruz Romero <romero.malissa@gmail.com>
2015-08-03 15:04:16 -0400

    post crud is functional, also added simple css

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+    #@post.order("RANDOM()").first
+    #Post.first(:order => "RANDOM()")
+    #Post.order("RANDOM()").first
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :command, :description)
+  end
+
+end
