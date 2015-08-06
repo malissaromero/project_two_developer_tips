7b62c2acf40d7638e99b4f41a6ca0e27b3658157
Malissa Cruz Romero <romero.malissa@gmail.com>
2015-08-06 16:09:55 -0400

    adding stuff

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+gem 'owlcarousel-rails'
 gem 'bcrypt'
 gem 'pry'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require owl.carousel
 //= require_tree .
+
+$(".owl-carousel").owlCarousel();
