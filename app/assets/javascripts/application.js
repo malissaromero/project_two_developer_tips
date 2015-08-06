3287ba9b29700d56e6472465494d409393d566f4
Malissa Cruz Romero <romero.malissa@gmail.com>
2015-08-06 15:52:59 -0400

    gem not working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bxslider
 //= require_tree .
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
+});
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,23 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate, only: [:index]
 
   def index
-    @categories = Category.all
-    @posts = Post.all
+    @user = User.find(session[:user_id])
+    if @user
+      @posts = Post.all
+      @categories = Category.all
+    else
+      @categories = Category.all
+    end
   end
 
   def show
     @category = Category.find(params[:id])
+    @post = Post.all
   end
 
+  private
+  def category_params
+    params.require(:category).permit(:name, :category_id)
+  end
 end
