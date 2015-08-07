fd3179cb2c91f4ea3c2c58e3bb86bab2bd0f6fb6
Malissa Cruz Romero <romero.malissa@gmail.com>
2015-08-06 21:27:50 -0400

    changed a category to atom

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Category.destroy_all
 Post.destroy_all
 
 ###### Post Seeds ######
- post1 = Post.create(title: "Find Your Current Directory", command: "pwd", description: "Print Working Directory allows you to see which directory you currently are in.")
+ #post1 = Post.create(title: "Find Your Current Directory", command: "pwd", description: "Print Working Directory allows you to see which directory you currently are in.")
 # post2 = Post.create(title: "", category: "", command: "", post_id: )
 # post3 = Post.create(title: "", category: "", command: "", post_id: )
 # post4 = Post.create(title: "", category: "", command: "", post_id: )
@@ -10,9 +10,5 @@ Post.destroy_all
 
 ###### Category Seeds ######
 beginner = Category.create(name: "Beginner")
-ruby = Category.create(name: "Ruby")
+ruby = Category.create(name: "Atom")
 terminal = Category.create(name: "Terminal")
-
-### CategoriesPosts Seeds ######
-# CategoriesPosts.create(post_id: @post1, category_id: @beginner)
-# CategoriesPosts.create(post_id:@post1, category_id: @terminal)
