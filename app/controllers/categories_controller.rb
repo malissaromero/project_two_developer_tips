b936cf35c512cbbb857ceb2b8829c44cdea583e9
mattscilipoti <matt.scilipoti@ga.co>
2015-08-13 08:18:48 -0400

    feedback from Matt: Good job.  See feedback_mms.md and inline-comments

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,16 @@
 class CategoriesController < ApplicationController
+  # mms: this is already in ApplicationController, so it a before_action for ALL controllers (that inherit from ApplicationController).
   before_action :authenticate
 
   def index
+    # mms: This code looks familiar - aren't we already finding the User from the session somwhere?
     @user = User.find(session[:user_id])
     if @user
       @posts = Post.all
       @categories = Category.all
+    # else
+      # mms: what should index do if there wasn't a current user?
     end
+
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,12 @@ class PostsController < ApplicationController
 
   def index
     @category = Category.find(params[:category_id])
+    # mms: don't you want the posts for the category you just retrieved?  Or is this carry-over from the previous implementation?
     @posts = Post.all
   end
 
   def new
+    # mms: Due to the nested routes, I was expecting a category here.  If not, I recommend you adjust your routes using `only:` or `except:`
     @post = Post.new
   end
 
@@ -24,12 +26,14 @@ class PostsController < ApplicationController
   end
 
   def update
+    # mms: we probably want to use `current_user.posts...` here too
     @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to post_path(@post)
   end
 
   def destroy
+    # mms: we probably want to use `current_user.posts...` here too
     @post = Post.find(params[:id])
     @post.destroy
     redirect_to categories_path
